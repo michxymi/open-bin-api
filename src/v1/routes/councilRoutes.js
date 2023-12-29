421e27f47f99610b49efea6d193f3976f228f2d5
michxymi <michxymi@gmail.com>
2023-12-29 16:08:08 +0000

    remove comments

--- src/v1/routes/councilRoutes.js
+++ src/v1/routes/councilRoutes.js
@@ -1,7 +1,6 @@
-// In src/v1/routes/workoutRoutes.js
 const express = require("express");
-const councilController = require("../../controllers/councilController");
 
+const councilController = require("../../controllers/councilController");
 const router = express.Router();
 
 router.get("/", councilController.getAllCouncils);
